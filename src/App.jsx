f49198a8b119f59d6451655d5e0ad33503f976a0
demitryedwards <dxyz773@gmail.com>
2023-09-09 15:30:31 -0400

    Country item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
@@ -6,7 +7,7 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
+import CountriesList from "./components/CountriesList";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -46,7 +47,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route
+            path="countries"
+            element={<CountriesList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,48 @@
+import CountryItem from "./CountryItem";
+import styles from "./CountriesList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CountriesList({ isLoading, cities }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message message="Add first city by clicking on a city on the map" />
+    );
+
+  const countryNames = cities
+    .map((city) => ({
+      country: city.country,
+      emoji: city.emoji,
+      id: city.id,
+    }))
+    .sort((a, b) => a.country.localeCompare(b.country));
+
+  let countries = [countryNames[0]];
+
+  for (const x in countryNames) {
+    if (countryNames[x]["country"] !== countries[0]["country"]) {
+      countries.unshift(countryNames[x]);
+    }
+  }
+
+  // Jonas's much more concise solution
+  // const countriesV2 = cities.reduce((arr, city) => {
+  //   if (!arr.map((el) => el.country).includes(city.country))
+  //     return [
+  //       ...arr,
+  //       { country: city.country, emoji: city.emoji, id: city.id },
+  //     ];
+  //   else return arr;
+  // }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountriesList;
