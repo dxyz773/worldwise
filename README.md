1e2b177771d7332d4d9138510cd892c38b894585
demitryedwards <dxyz773@gmail.com>
2023-09-09 20:46:01 -0400

    Add Button component. Update Form, Map and README

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 - Work in progress 👩🏾‍💻
 
 #### Made while taking Jonas Schmedtmann's - Ultimate React Course
+
+## A User can:
+
+## Topics of Exploration:
+
+1. React Router 6
+2. useParams
+3. useSearchParams
+4. useNavigate
+5. < Navigate />
+6. CSS Modules
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
@@ -7,6 +7,8 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
+import City from "./components/City";
+import Form from "./components/Form";
 import CountriesList from "./components/CountriesList";
 
 const BASE_URL = "http://localhost:9000";
@@ -39,19 +41,17 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountriesList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
